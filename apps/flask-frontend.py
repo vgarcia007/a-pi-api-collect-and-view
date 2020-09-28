d0ead241d7aa9dad4775e4a020701d14d84fceae
Philipp <philipp@freilinger.org>
2020-09-28 11:58:13 +0200

    navigation und charts

--- apps/flask-frontend.py
+++ apps/flask-frontend.py
@@ -2,18 +2,14 @@ import sqlite3
 from sqlite3 import Error
 from common.devices import devices
 from common.functions import get_api, get_and_save_value_from_api, create_connection, script_dir, db_file
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+from datetime import datetime
 
 
 app = Flask(__name__, static_folder="static_files")
 
 @app.route('/')
 def home():
-    conn = create_connection(db_file)
-    c = conn.cursor()
-    sql = ''' SELECT * FROM records'''
-    c.execute(sql)
-    db_data = c.fetchall()
 
     api_data=[]
 
@@ -29,7 +25,18 @@ def home():
         api_data.append(info)
         i=i+1
 
-    return render_template('home.html', data=db_data, api_data=api_data)
+    return render_template('home.html', api_data=api_data)
+
+
+@app.route('/db_test')
+def db_test():
+    conn = create_connection(db_file)
+    c = conn.cursor()
+    sql = ''' SELECT * FROM records'''
+    c.execute(sql)
+    db_data = c.fetchall()
+
+    return render_template('db_test.html', data=db_data)
 
 @app.route('/pi/<pi>')
 def pi(pi):
@@ -88,6 +95,54 @@ def pi(pi):
         return render_template('error.html', text='device not in list')
 
     return render_template('pi.html', device_name=device_name ,data=latest_data)
+
+
+@app.route('/chart/<pi>/<sensor>')
+def chart(pi, sensor):
+
+    conn = create_connection(db_file)
+
+    for device in devices:
+        if device['name'] == pi:
+            
+            device_name= pi
+
+            sensor_name = sensor
+
+    return render_template('chart.html', device_name=device_name,pi=pi,sensor_name=sensor_name)
+
+@app.route('/chart/json/<pi>/<sensor>')
+def chart_json(pi, sensor):
+
+    conn = create_connection(db_file)
+    conn.row_factory = sqlite3.Row
+
+    json = []
+
+    for device in devices:
+        if device['name'] == pi:
+            
+            sensor_name = sensor
+
+            info = get_api(device['ip'])
+
+            c = conn.cursor()
+            sql = ''' SELECT data, recorded FROM records WHERE hostname = ? AND name = ? ORDER BY recorded DESC'''
+            c.execute(sql, (info['hostname'], sensor_name))
+            db_data = c.fetchall()
+
+            
+            for data in db_data:
+                item = {
+                    'value': float(data[0]),
+                    'key':datetime.utcfromtimestamp(data[1]).strftime('%Y-%m-%d %H:%M:%S')
+                    }
+
+                json.append(item)
+                
+
+
+    return jsonify(json)
 
 if __name__ == '__main__':
 
